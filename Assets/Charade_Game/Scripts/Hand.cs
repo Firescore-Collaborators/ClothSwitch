76d661008226efd994362eede0acaf4bc7d819c1
cshoaib05 <choudharyshoaib05@gmail.com>
2021-09-07 11:46:39 +0530

    Added'

---
+++ Assets/Charade_Game/Scripts/Hand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    Vector3 handpos;
+
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        handpos = Input.mousePosition;
+        handpos.y = handpos.y - 25f;
+        transform.position = handpos;
+    }
+}
