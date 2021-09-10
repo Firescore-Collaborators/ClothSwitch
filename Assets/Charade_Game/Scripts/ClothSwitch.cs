700512ee13e06005966e9aa3aaeb37f2d03b0895
cshoaib05 <choudharyshoaib05@gmail.com>
2021-09-10 12:11:17 +0530

    Added

--- Assets/Charade_Game/Scripts/ClothSwitch.cs
+++ Assets/Charade_Game/Scripts/ClothSwitch.cs
@@ -7,8 +7,9 @@ public class ClothSwitch : MonoBehaviour
     public GameObject clothSelected;
     public GameObject pareo;
     public GameObject hat;
+    public GameObject shirt;
     RaycastHit hit;
-
+    string cloth = "";
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -18,12 +19,20 @@ public class ClothSwitch : MonoBehaviour
             {
                 if (hit.transform.name == "Pareo")
                 {
+                    cloth = "Pareo";
                     clothSelected = hit.transform.gameObject;
                 }
 
                 if (hit.transform.name == "Hat")
                 {
+                    cloth = "Hat";
                     clothSelected = hit.transform.gameObject; 
+                }
+
+                if (hit.transform.name == "Shirt")
+                {
+                    cloth = "Shirt";
+                    clothSelected = hit.transform.gameObject;
                 }
                 clothSelected.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(4f, 4f, 4f));
             }
@@ -32,7 +41,20 @@ public class ClothSwitch : MonoBehaviour
         if(Input.GetMouseButtonUp(0))
         {
             clothSelected.SetActive(false);
-            pareo.SetActive(true);
+            if(cloth == "Pareo")
+            {
+                pareo.SetActive(true);
+            }
+
+            if(cloth == "Hat")
+            {
+                hat.SetActive(true);
+            }
+
+            if (cloth == "Shirt")
+            {
+                shirt.SetActive(true);
+            }
         }
     }
 }
