c5c1de35842ef82ba2057258c172e99b0b1b4749
cshoaib05 <choudharyshoaib05@gmail.com>
2021-09-14 15:33:25 +0530

    addded

--- Assets/Charade_Game/Scripts/ClothSwitch.cs
+++ Assets/Charade_Game/Scripts/ClothSwitch.cs
@@ -7,9 +7,37 @@ public class ClothSwitch : MonoBehaviour
     public GameObject clothSelected;
     public GameObject pareo;
     public GameObject hat;
+    public GameObject Bra;
+    public GameObject Fishnet;
     public GameObject shirt;
+
+    public GameObject Mpareo;
+    public GameObject MBra;
+    public GameObject MFishnet;
+    public GameObject Mshirt;
+    public GameObject Mshots;
+
+
+    public GameObject Fpareo;
+    public GameObject Fhat;
+    public GameObject FBra;
+    public GameObject FFishnet;
+    public GameObject Fshirt;
+    public GameObject Fshots;
+
+    public GameObject Female;
+
+    public GameObject pShirt;
+    public GameObject Pshots;
+    public GameObject pBra;
+    public GameObject pFishnet;
+
+    bool done = false;
     RaycastHit hit;
     string cloth = "";
+    string character = "";
+    int count = 0;
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -17,43 +45,144 @@ public class ClothSwitch : MonoBehaviour
         {
             if (Physics.Raycast(ray, out hit, 100))
             {
-                if (hit.transform.name == "Pareo")
+                if(!done)
                 {
-                    cloth = "Pareo";
-                    clothSelected = hit.transform.gameObject;
-                }
+                    print(hit.transform.name);
+                    if (hit.transform.name == "Pareo")
+                    {
+                        done = true;
+                        cloth = "Pareo";
+                        clothSelected = hit.transform.gameObject;
+                    }
 
-                if (hit.transform.name == "Hat")
-                {
-                    cloth = "Hat";
-                    clothSelected = hit.transform.gameObject; 
-                }
+                    if (hit.transform.name == "Hat")
+                    {
 
-                if (hit.transform.name == "Shirt")
-                {
-                    cloth = "Shirt";
-                    clothSelected = hit.transform.gameObject;
+                        done = true;
+                        cloth = "Hat";
+                        clothSelected = hit.transform.gameObject;
+                    }
+
+                    if (hit.transform.name == "Bra")
+                    {
+
+                        done = true;
+                        cloth = "Bra";
+                        clothSelected = hit.transform.gameObject;
+                    }
+
+                    if (hit.transform.name == "Shirt")
+                    {
+
+                        done = true;
+                        cloth = "Shirt";
+                        clothSelected = hit.transform.gameObject;
+                    }
+
+                    if (hit.transform.name == "Fishnet")
+                    {
+
+                        done = true;
+                        cloth = "Fishnet";
+                        clothSelected = hit.transform.gameObject;
+                    }
+
+                    if (hit.transform.name == "FShirt")
+                    {
+                        LeanTween.rotateLocal(Female, new Vector3(0f, -210f, 0f), 0.2f);
+                        done = true;
+                        cloth = "FShirt";
+                        Fshirt.SetActive(false);
+                        clothSelected = Instantiate(pShirt, hit.transform.position, hit.transform.rotation);
+                    }
+
+                    if (hit.transform.name == "Fshots")
+                    {
+                        done = true;
+                        cloth = "Fshots";
+                        Fshots.SetActive(false);
+                        clothSelected = Instantiate(Pshots, hit.transform.position, hit.transform.rotation);
+                    }
+
+
+                    if (hit.transform.name == "MFishnet")
+                    {
+                        done = true;
+                        cloth = "MFishnet";
+                        MFishnet.SetActive(false);
+                        clothSelected = Instantiate(pFishnet, hit.transform.position, hit.transform.rotation);
+                    }
+
+                    if (hit.transform.name == "MBra")
+                    {
+                        done = true;
+                        cloth = "MBra";
+                        MBra.SetActive(false);
+                        clothSelected = Instantiate(pBra, hit.transform.position, hit.transform.rotation);
+                    }
                 }
                 clothSelected.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(4f, 4f, 4f));
             }
+
         }
 
         if(Input.GetMouseButtonUp(0))
         {
-            clothSelected.SetActive(false);
+            done = false;
+            clothSelected.gameObject.SetActive(false);
             if(cloth == "Pareo")
             {
-                pareo.SetActive(true);
+                Fpareo.SetActive(true);
+                count++;
             }
 
             if(cloth == "Hat")
             {
-                hat.SetActive(true);
+                Fhat.SetActive(true);
+                count++;
+            }
+            if (cloth == "Bra" && count == 0)
+            {
+                MBra.SetActive(true);
+                count++;
             }
 
             if (cloth == "Shirt")
             {
+                Mshirt.SetActive(true);
+                count++;
+            }
+
+            if (cloth == "Fishnet")
+            {
+                Fishnet.SetActive(true);
+                count++;
+            }
+
+            if (cloth == "FShirt")
+            {
                 shirt.SetActive(true);
+                count++;
+            }
+
+            if (cloth == "Fshots")
+            {
+                Mshots.SetActive(true);
+                count++;
+            }
+
+            if (cloth == "MFishnet")
+            {
+                FFishnet.SetActive(true);
+                count++;
+            }
+
+            if (cloth == "MBra")
+            {
+                FBra.SetActive(true);
+                MFishnet.GetComponent<BoxCollider>().enabled = true;
+                LeanTween.rotateLocal(Female, new Vector3(0f, -23f, 0f), 0.2f);
+                count++;
             }
         }
     }
